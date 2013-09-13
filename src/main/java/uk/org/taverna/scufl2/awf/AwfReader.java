eff4e584e4e7d44173852852a9d16d45c4e4860a
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-09-13 17:06:58 +0100

    Reading in MG-RAST AWE awf
    Examples from https://github.com/MG-RAST/AWE/tree/master/templates/awf_templates

---
+++ src/main/java/uk/org/taverna/scufl2/awf/AwfReader.java
@@ -0,0 +1,175 @@
+package uk.org.taverna.scufl2.awf;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import uk.org.taverna.scufl2.api.common.Scufl2Tools;
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.BlockingControlLink;
+import uk.org.taverna.scufl2.api.core.DataLink;
+import uk.org.taverna.scufl2.api.core.Processor;
+import uk.org.taverna.scufl2.api.core.Workflow;
+import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+import uk.org.taverna.scufl2.api.io.WorkflowBundleReader;
+import uk.org.taverna.scufl2.api.port.InputProcessorPort;
+import uk.org.taverna.scufl2.api.port.InputWorkflowPort;
+import uk.org.taverna.scufl2.api.port.OutputProcessorPort;
+import uk.org.taverna.scufl2.api.port.SenderPort;
+
+public class AwfReader implements WorkflowBundleReader {
+
+    private static final String TASK = "task_";
+    private static Scufl2Tools scufl2Tools = new Scufl2Tools();
+    private static WorkflowBundleIO bundleIO = new WorkflowBundleIO();
+    private static final String AWF_JSON = "text/vnd.mgrast.awe.awf+json";
+    private static final Charset LATIN1 = Charset.forName("latin1");
+    private static ObjectMapper om = new ObjectMapper();
+    
+    public Set<String> getMediaTypes() {
+        return Collections.singleton(AWF_JSON);
+    }
+
+    public WorkflowBundle readBundle(File bundleFile, String mediaType)
+            throws ReaderException, IOException {        
+        JsonNode json = om.readTree(bundleFile);
+        return readJson(json);
+    }
+
+    public WorkflowBundle readBundle(InputStream inputStream, String mediaType)
+            throws ReaderException, IOException {
+        JsonNode json = om.readTree(inputStream);
+        return readJson(json);
+    }
+
+    public WorkflowBundle readJson(JsonNode json) {
+        WorkflowBundle bundle = bundleIO.createBundle();
+        parseWorkflowInfo(json.get("workflow_info"), bundle);
+        parseRawInputs(json.get("raw_inputs"), bundle.getMainWorkflow());
+        parseVariables(json.get("variables"), bundle);
+        parseTasks(json.get("tasks"), bundle);
+        return bundle;
+    }
+
+    private void parseTasks(JsonNode jsonNode, WorkflowBundle bundle) {
+        if (jsonNode == null) {
+            return;
+        }
+        for (JsonNode task : jsonNode) {
+            String name = TASK + task.get("taskid").asText();
+            
+            Workflow workflow = bundle.getMainWorkflow();
+            Processor proc = createProcessorIfNotExist(workflow, name);
+            // TODO: parse input ports
+            JsonNode inputs = task.get("inputs");
+            for (String inputPort : iterate(inputs.fieldNames())) {
+                InputProcessorPort in = new InputProcessorPort(proc, inputPort);
+                int source = inputs.get(inputPort).asInt();
+                SenderPort senderPort;
+                if (source == 0) {
+                    senderPort = workflow.getInputPorts().getByName(inputPort);
+                } else {
+                    Processor sourceProc = createProcessorIfNotExist(workflow, TASK + source);
+                    senderPort = createOutputPortIfNotExists(sourceProc, inputPort);
+                }
+                if (senderPort != null) { 
+                    DataLink dataLink = new DataLink(workflow, senderPort, in);
+                }
+            }
+
+            for (JsonNode portNode : task.get("outputs")) {
+                OutputProcessorPort out = new OutputProcessorPort(proc, portNode.asText());
+            }
+            for (JsonNode dependsOn : task.get("dependsOn")) {
+                if (dependsOn.asText().equals("0")) {
+                    // Not sure what is the point of this dependsOn
+                    continue;
+                }
+                Processor waitFor = createProcessorIfNotExist(workflow, TASK + dependsOn.asText());
+                new BlockingControlLink(proc, waitFor);
+            }
+            // TODO: "splits": 8  ??
+ 
+            
+            scufl2Tools.createActivityFromProcessor(proc, bundle.getMainProfile());
+            // TODO: Parse cmd and args
+        }
+    }
+
+    private OutputProcessorPort createOutputPortIfNotExists(Processor proc,
+            String name) {
+        OutputProcessorPort port = proc.getOutputPorts().getByName(name);
+        if (port == null) {
+            port = new OutputProcessorPort(proc, name);
+        }
+        return port;        
+    }
+
+    
+    private Processor createProcessorIfNotExist(Workflow workflow, String name) {
+        Processor proc = workflow.getProcessors().getByName(name);
+        if (proc == null) {
+            proc = new Processor(workflow, name);
+        }
+        return proc;
+    }
+
+    private void parseVariables(JsonNode jsonNode, WorkflowBundle bundle) {
+        if (jsonNode == null) {
+            return;
+        }
+        for (String variable : iterate(jsonNode.fieldNames())) {
+            JsonNode variableValue = jsonNode.get(variable);
+            
+            Processor constant = scufl2Tools.createConstant(bundle.getMainWorkflow(), bundle.getMainProfile(), variable);
+            scufl2Tools.setConstantStringValue(constant, variableValue.asText(), bundle.getMainProfile());            
+        }
+    }
+
+    private void parseRawInputs(JsonNode jsonNode, Workflow workflow) {
+        if(jsonNode == null) {
+            return;
+        }
+        for (String input : iterate(jsonNode.fieldNames())) {
+            InputWorkflowPort inputPort = new InputWorkflowPort(workflow, input);
+            inputPort.setParent(workflow);
+        }
+    }
+
+    private <T> Iterable<T> iterate(final Iterator<T> iterator) {
+        return new Iterable<T>() {
+            public Iterator<T> iterator() {
+                return iterator;
+            }
+        };
+    }
+
+    private void parseWorkflowInfo(JsonNode jsonNode, WorkflowBundle bundle) {
+        if (jsonNode == null) { 
+            return;
+        }
+        JsonNode name = jsonNode.get("name");
+        if (name != null) {
+            bundle.setName(name.asText());
+            bundle.getMainWorkflow().setName(name.asText());
+        }
+        // TODO: Also copy author/description annotations
+    }
+
+    public String guessMediaTypeForSignature(byte[] firstBytes) {
+        String s = new String(firstBytes, LATIN1).trim();
+        if (s.startsWith("{") && s.contains("\"workflow_info\"")) {
+            return AWF_JSON;
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/uk/org/taverna/scufl2/awf/TestAwfReader.java
@@ -0,0 +1,86 @@
+package uk.org.taverna.scufl2.awf;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import uk.org.taverna.scufl2.api.common.Scufl2Tools;
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.BlockingControlLink;
+import uk.org.taverna.scufl2.api.core.DataLink;
+import uk.org.taverna.scufl2.api.core.Processor;
+import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+import uk.org.taverna.scufl2.api.port.InputProcessorPort;
+import uk.org.taverna.scufl2.api.port.InputWorkflowPort;
+import uk.org.taverna.scufl2.api.port.OutputProcessorPort;
+import uk.org.taverna.scufl2.api.port.ReceiverPort;
+
+public class TestAwfReader {
+    private static WorkflowBundleIO bundleIO = new WorkflowBundleIO();
+    private static Scufl2Tools scufl2Tools = new Scufl2Tools();
+
+    
+    @Test
+    public void readMgrastShort() throws Exception {       
+        WorkflowBundle bundle = bundleIO.readBundle(getClass().getResource("/mgrast-short.awf"), null);
+        
+        InputWorkflowPort wfIn = bundle.getMainWorkflow().getInputPorts().getByName("#i_1");
+        assertNotNull(wfIn);
+        
+        Set<Processor> constants = scufl2Tools.getConstants(bundle.getMainWorkflow(), bundle.getMainProfile());
+        assertEquals(5, constants.size());
+        Processor fgs_type = bundle.getMainWorkflow().getProcessors().getByName("fgs_type");
+        assertEquals("454", scufl2Tools.getConstantStringValue(fgs_type, bundle.getMainProfile()));
+        
+        Set<String> processors = bundle.getMainWorkflow().getProcessors().getNames();  
+        System.out.println(processors);
+        assertEquals(6+constants.size(), processors.size());
+        assertTrue(processors.contains("task_1"));
+        assertTrue(processors.contains("task_2")); // ..
+        assertTrue(processors.contains("task_6"));
+        Processor task1 = bundle.getMainWorkflow().getProcessors().getByName("task_1");
+        InputProcessorPort procIn = task1.getInputPorts().first();
+        assertEquals("#i_1", procIn.getName());
+        assertEquals(wfIn, scufl2Tools.datalinksTo(procIn).get(0).getReceivesFrom());
+        
+        OutputProcessorPort out = task1.getOutputPorts().getByName("prep.passed.fna");        
+        List<DataLink> linksFrom = scufl2Tools.datalinksFrom(out);
+        assertEquals(1, linksFrom.size());
+        ReceiverPort linksTo = linksFrom.get(0).getSendsTo();
+        assertEquals("prep.passed.fna", linksTo.getName());
+        
+        Processor task2 = ((InputProcessorPort) linksTo).getParent();
+        assertEquals("task_2", task2.getName());
+        
+        List<BlockingControlLink> blocking = scufl2Tools.controlLinksBlocking(task2);
+        assertEquals(1, blocking.size());
+        assertEquals(task1, blocking.get(0).getUntilFinished());
+        
+        // TODO: Check cmd 
+        
+    }
+    
+    @Test
+    public void pipelineExampleAsWfBundle() throws Exception {       
+        WorkflowBundle bundle = bundleIO.readBundle(getClass().getResource("/pipeline_example.awf"), null);
+        Path dest = Files.createTempFile("test", ".wfbundle");
+        bundleIO.writeBundle(bundle, dest.toFile(), "application/vnd.taverna.scufl2.workflow-bundle");
+        System.out.println("Written to " + dest);
+    }
+    
+    @Test
+    public void workflowExampleAsWfdesc() throws Exception {       
+        WorkflowBundle bundle = bundleIO.readBundle(getClass().getResource("/workflow_example.awf"), null);
+        Path dest = Files.createTempFile("test", ".wfdesc.ttl");
+        bundleIO.writeBundle(bundle, dest.toFile(), "text/vnd.wf4ever.wfdesc+turtle");
+        System.out.println("Written to " + dest);
+    }
+    
+}
